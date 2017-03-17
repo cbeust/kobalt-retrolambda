c576ff9a7f0aa1a9216cea00adaa71dcd3b76a42
Cedric Beust <cedric.beust@le.com>
2017-03-17 12:31:32 -0700

    1.0.9.

--- kobalt/src/Build.kt
+++ kobalt/src/Build.kt
@@ -7,7 +7,7 @@ val p = project {
     name = "kobalt-retrolambda"
     artifactId = name
     group = "com.beust"
-    version = "1.0.8"
+    version = "1.0.9"
 
     dependencies {
       compile("com.beust:kobalt-plugin-api:1.0.8")
