ad871705487c00ce7247f0966572ff9f6ff426ef
Cedric Beust <cedric.beust@le.com>
2017-03-17 12:29:46 -0700

    Kobalt update.

--- kobalt/src/Build.kt
+++ kobalt/src/Build.kt
@@ -2,21 +2,15 @@
 import com.beust.kobalt.plugin.packaging.assemble
 import com.beust.kobalt.plugin.publish.bintray
 import com.beust.kobalt.project
-import com.beust.kobalt.repos
-
-val bs = buildScript {
-    val r = repos("https://dl.bintray.com/cbeust/maven")
-    //val pl = plugins(file(homeDir("kotlin/kobalt-retrolambda/kobaltBuild/libs/kobalt-retrolambda-0.3.jar")))
-}
 
 val p = project {
     name = "kobalt-retrolambda"
     artifactId = name
     group = "com.beust"
-    version = ".939"
+    version = "1.0.8"
 
     dependencies {
-      compile("com.beust:kobalt-plugin-api:0.939")
+      compile("com.beust:kobalt-plugin-api:1.0.8")
     }
 
     assemble {
